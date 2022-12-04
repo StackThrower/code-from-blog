e8e05a22ee75f52c0e4cc4736188480088ded103
Vlad <0x01code@gmail.com>
2022-12-04 17:38:53 +0200

    Substring in String class.

---
+++ SubstringFromStringInJava.java
@@ -0,0 +1,8 @@
+public class SubstringFromStringInJava {
+
+    public static void main(String[] params) {
+        String stringOne = "Stack Throw";
+
+        System.out.println(stringOne.substring(0, 5));
+    }
+}
